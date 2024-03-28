afc3f3668dc15e31ded0d51fb8df81d8ac6f0f03
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2024-03-28 15:35:39 +0000

    59051abe-ac76-4265-9144-1b945edb0afb

--- src/dotnet/Azd.RxTx.Processor.v2/Worker.cs
+++ src/dotnet/Azd.RxTx.Processor.v2/Worker.cs
@@ -44,7 +44,7 @@ public class Worker : BackgroundService
 
             _processor.Enqueue(batch);
 
-            await Task.Delay(500, stoppingToken);
+            await Task.Delay(200, stoppingToken);
         }
     }
 
