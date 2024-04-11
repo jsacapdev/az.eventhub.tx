862387add7a967496e2edae67bf187462ab2b45a
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2024-04-11 14:56:24 +0100

    d6c80729-d4de-4d01-b52c-92e61a43631f

--- src/dotnet/tests/Azd.RxTx.Processor.v2.Tests/MessageProcessorTests.cs
+++ src/dotnet/tests/Azd.RxTx.Processor.v2.Tests/MessageProcessorTests.cs
@@ -30,4 +30,37 @@ public class MessageProcessorTests
                                                                         mockConfiguration.Object,
                                                                         client));
     }
+
+    [Fact]
+    public void Can_Process_Message_Single_Thread_OK()
+    {
+        var reset = new AutoResetEvent(false);
+        var mockLogger = new Mock<ILogger<MessageProcessor>>();
+        var mockHostApplicationLifetime = new Mock<IHostApplicationLifetime>();
+        var config = TelemetryConfiguration.CreateDefault();
+        var client = new TelemetryClient(config);
+
+        var mockConfiguration = new Mock<IConfiguration>();
+        mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "MessageProcessorThreadCount")]).Returns("1");
+
+        var mockSender = new Mock<IMessageSender<MessageBatch<string>>>();
+        mockSender.Setup(sender => sender.SendBatchAsync(It.IsAny<MessageBatch<string>>()))
+                  .Callback(() =>
+                  {
+                      reset.Set();
+                  });
+
+        var processor = new MessageProcessor(mockLogger.Object,
+                                             mockSender.Object,
+                                             mockHostApplicationLifetime.Object,
+                                             mockConfiguration.Object,
+                                             client);
+        processor.Initialize();
+
+        processor.Enqueue(new(["mockData"]));
+
+        var wasSignaled = reset.WaitOne(timeout: TimeSpan.FromSeconds(60));
+
+        mockSender.Verify(sender => sender.SendBatchAsync(It.IsAny<MessageBatch<string>>()), Times.AtMostOnce());
+    }
 }
